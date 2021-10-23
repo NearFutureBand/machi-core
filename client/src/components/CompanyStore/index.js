97c7d157ecd64b50a7245ef2cc96f02464de5399
Pavel Belyakov <theflashofsonic@gmail.com>
2021-10-23 16:41:24 +0300

    a bit more simple modal window logic

--- client/src/components/CompanyStore/index.js
+++ client/src/components/CompanyStore/index.js
@@ -3,22 +3,25 @@ import { memo, useMemo } from "react";
 import CARDS from "../../constants/cards.json";
 import { CARD_IMAGES } from "../../assets";
 import { Card } from "../Card";
-import "./styles.css";
+import "./styles.scss";
 
 const CompanyStore = memo(({ onClose }) => {
 
   const companies = Object.values(CARDS).filter(card => card.price !== 0);
   
   return (
-    <div className="modal-background" onClick={onClose}>
+    <div className="modal-background">
       <div className="modal">
+        <button className="button-close" onClick={onClose}>X</button>
         <div className="company-store">
           {companies.map((company) => {
             return (
               <Card id={company.id} key={company.id} />
             )
           })}
+          
         </div>
+        <button onClick={onClose}>Закрыть</button>
       </div>
     </div>
   )
